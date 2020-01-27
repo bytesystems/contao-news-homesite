ffebeb004e41738a82d1faecd155c1b7f718e845
danielheim <heim@bytesystems.de>
2020-01-27 13:51:28 +0100

    Initial Commit

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -21,6 +21,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use bits\ContaoNewsHomesite\ContaoNewsHomesiteBundle;
 
 class Plugin implements BundlePluginInterface
 {
